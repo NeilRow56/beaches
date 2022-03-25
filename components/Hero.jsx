42bc733302e7b1a8bcaf69fc385553bed440b523
Neil <neilrowland56@yahoo.com>
2022-03-25 16:28:53 +0000

    Hero section added with video background and search form

---
+++ components/Hero.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { AiOutlineSearch } from 'react-icons/ai';
+
+function Hero() {
+  return (
+    <div className='h-screen w-full relative'>
+        <video
+        
+      className='w-full h-full object-cover'
+       autoPlay
+        loop
+        muted
+       
+        >
+        <source src="/img/beachVid.mp4" type="video/mp4"/>
+        </video>
+        {/* Overlay */}
+        <div className='absolute w-full h-full top-0 bg-gray-900/30'></div>
+        <div className='absolute top-0 w-full h-full flex flex-col justify-center text-center text-white p-4'>
+          <h1>First Class Travel</h1>
+          <h2 className='py-4'>Top Locations Worldwide</h2>
+          <form className='flex justify-between items-center max-w-[700px] mx-auto w-full border p-1 rounded-md text-black bg-gray-100/90'>
+            <div>
+              <input className='bg-transparent w-[300px] sm:w-[400px] focus:outline-none' type="text" placeholder='Search Destinations'/>
+            </div>
+            <div>
+              <button><AiOutlineSearch size={20} className='icon' style={{color:'#ffff'}}/></button>
+            </div>
+          </form>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Hero
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import Hero from '../components/Hero';
 import Navbar from '../components/Navbar';
 
 export default function Home() {
@@ -15,6 +16,7 @@ export default function Home() {
 			</Head>
 			<div>
 				<Navbar />
+				<Hero />
 			</div>
 		</div>
 	);
